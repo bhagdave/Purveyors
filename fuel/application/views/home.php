a995eba3c59272b8c31f2665e76f402e86878847
Dave Gill <dave_gill@blueyonder.co.uk>
2014-09-11 15:10:35 +0100

    Fixed typo

--- fuel/application/views/home.php
+++ fuel/application/views/home.php
@@ -1,3 +1,3 @@
 <h1>Welcome</h1>
-<p>Purveyors of Evil is a worldwide organisation dedicated to the eradication of the human race. We are confident in our belief that the world would be a better place without us.  So we are concentrating our efforts on ensuring that all living creatures ar efree from the scourge that is the human race.</p>
+<p>Purveyors of Evil is a worldwide organisation dedicated to the eradication of the human race. We are confident in our belief that the world would be a better place without us.  So we are concentrating our efforts on ensuring that all living creatures are free from the scourge that is the human race.</p>
 <p>Not really!</p>
